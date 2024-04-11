84d8b614325fcdf2bac4acdcde0158a0ff7da43d
Taka <takahito.uchino.1218@gmail.com>
2024-04-11 13:29:53 +0900

    tessoku A18

--- tessoku-book/src/bin/a18.rs
+++ tessoku-book/src/bin/a18.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: usize,
+        a: [usize; n],
+    }
+
+    let mut dp = vec![vec![false;s + 1]; n + 1];
+    dp[0][0] = true;
+
+    for i in 1..=n {
+        for j in 0..=s {
+            if j < a[i - 1] {
+                if dp[i - 1][j] {
+                    dp[i][j] = true;
+                }
+            }
+            if j >= a[i - 1] {
+                if dp[i - 1][j] || dp[i - 1][j - a[i - 1]] {
+                    dp[i][j] = true;
+                }
+            }
+        }
+    }
+
+    println!("{}", if dp[n][s] {"Yes"} else {"No"});
 }
