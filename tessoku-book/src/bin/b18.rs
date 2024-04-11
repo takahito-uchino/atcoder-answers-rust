664eb46b644dfd644f6ce3a66be8b6553cd79608
Taka <takahito.uchino.1218@gmail.com>
2024-04-11 13:45:23 +0900

    tessoku B18

--- tessoku-book/src/bin/b18.rs
+++ tessoku-book/src/bin/b18.rs
@@ -1,3 +1,49 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: usize,
+        a: [usize; n],
+    }
+
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+
+    for i in 1..=n {
+        for j in 0..=s {
+            if j < a[i - 1] {
+                if dp[i - 1][j] {
+                    dp[i][j] = true;
+                }
+            }
+            if j >= a[i - 1] {
+                if dp[i - 1][j] || dp[i - 1][j - a[i - 1]] {
+                    dp[i][j] = true;
+                }
+            }
+        }
+    }
+
+    if !dp[n][s] {
+        println!("-1");
+        return;
+    }
+
+    let mut card = s;
+    let mut answer = Vec::new();
+
+    for i in (1..=n).rev() {
+        if dp[i - 1][card] {
+            card = card - 0;
+        } else {
+            card -= a[i - 1];
+            answer.push(i);
+        }
+    }
+
+    answer.reverse();
+
+    println!("{}", answer.len());
+    println!("{}", answer.iter().map(|n| n.to_string()).collect::<Vec<_>>().join(" "));
 }
