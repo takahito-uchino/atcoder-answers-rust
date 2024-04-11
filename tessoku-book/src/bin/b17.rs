5281fb6057d7952f55bc3307e4423756874082e4
Taka <takahito.uchino.1218@gmail.com>
2024-04-11 13:15:25 +0900

    tessoku B17

--- tessoku-book/src/bin/b17.rs
+++ tessoku-book/src/bin/b17.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        h: [isize; n],
+    }
+
+    let mut dp = Vec::new();
+    dp.push(0);
+    dp.push((h[0] - h[1]).abs());
+
+    for i in 2..n {
+        dp.push(((h[i] - h[i - 1]).abs() + dp[i - 1]).min((h[i] - h[i - 2]).abs() + dp[i - 2]));
+    }
+
+    let mut place = n - 1;
+    let mut answer = Vec::new();
+
+    loop {
+        answer.push(place + 1);
+        if place == 0 {
+            break;
+        }
+        if dp[place - 1] + (h[place - 1] - h[place]).abs() == dp[place] {
+            place -= 1;
+        } else {
+            place -= 2;
+        }
+    }
+
+    answer.reverse();
+
+    println!("{}", answer.len());
+    println!("{}", answer.iter().map(|n| n.to_string()).collect::<Vec<_>>().join(" "));
 }
+
