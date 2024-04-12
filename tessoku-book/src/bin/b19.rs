583c22aeeb549f43c8c3b6b07abece326d58495c
Taka <takahito.uchino.1218@gmail.com>
2024-04-12 10:33:39 +0900

    tessoku b19

--- tessoku-book/src/bin/b19.rs
+++ tessoku-book/src/bin/b19.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, usize); n],
+    }
+
+    let mut dp = vec![vec![1_000_000_000_000_000; 100001]; n + 1];
+    dp[0][0] = 0;
+
+    for i in 1..=n {
+        for j in 0..100001 {
+            if j < wv[i - 1].1 {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = (dp[i - 1][j]).min(dp[i - 1][j - wv[i - 1].1] + wv[i - 1].0);
+            }
+        }
+    }
+
+    let mut answer = 0;
+    for i in 0..100001 {
+        if dp[n][i] <= w {
+            answer = i;
+        }
+    }
+    println!("{}", answer);
 }
