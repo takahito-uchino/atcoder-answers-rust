e49ab36dbd17ad7d0de90f0060b9a67554593696
Taka <takahito.uchino.1218@gmail.com>
2024-04-12 11:12:35 +0900

    tessoku B20

--- tessoku-book/src/bin/b20.rs
+++ tessoku-book/src/bin/b20.rs
@@ -1,3 +1,30 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    let n = s.len();
+    let m = t.len();
+    let mut dp = vec![vec![20000; m + 1]; n + 1];
+
+    dp[0][0] = 0;
+
+    for i in 0..=n {
+        for j in 0..=m {
+            if i >= 1 && j >= 1 && s[i - 1] == t[j - 1] {
+                dp[i][j] = (dp[i - 1][j] + 1).min(dp[i][j - 1] + 1).min(dp[i - 1][j - 1]);
+            } else if i >= 1 && j >= 1 {
+                dp[i][j] = (dp[i - 1][j] + 1).min(dp[i][j - 1] + 1).min(dp[i - 1][j - 1] + 1);
+            } else if i >= 1 {
+                dp[i][j] = dp[i - 1][j] + 1;
+            } else if j >= 1 {
+                dp[i][j] = dp[i][j - 1] + 1;
+            }
+        }
+    }
+
+    println!("{}", dp[n][m]);
 }
