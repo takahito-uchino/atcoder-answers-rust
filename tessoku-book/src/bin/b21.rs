6b12ab1de0dc45aaa8b45cbbaa8d7ea2e40d895e
Taka <takahito.uchino.1218@gmail.com>
2024-04-13 13:35:33 +0900

    tessoku B21

--- tessoku-book/src/bin/b21.rs
+++ tessoku-book/src/bin/b21.rs
@@ -1,3 +1,35 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    let mut dp = vec![vec![0; n + 1]; n + 1];
+
+    for i in 0..n {
+        dp[i][i] = 1;
+    }
+    for i in 0..n - 1 {
+        if s[i] == s[i + 1] {
+            dp[i][i + 1] = 2;
+        } else {
+            dp[i][i + 1] = 1;
+        }
+    }
+
+    for len in 2..=n - 1 {
+        for l in 0..n - len {
+            let r = l + len;
+
+            if s[l] == s[r] {
+                dp[l][r] = (dp[l][r - 1]).max(dp[l + 1][r]).max(dp[l + 1][r - 1] + 2);
+            } else {
+                dp[l][r] = (dp[l][r - 1]).max(dp[l + 1][r]);
+            }
+        }
+    }
+
+    println!("{}", dp[0][n - 1]);
 }
