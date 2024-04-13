fe0b79709e30ef2da5aeb6817e4ff7ba125a9c87
Taka <takahito.uchino.1218@gmail.com>
2024-04-13 21:18:07 +0900

    ABC B

--- abc349/src/bin/b.rs
+++ abc349/src/bin/b.rs
@@ -1,3 +1,30 @@
+use im_rc::HashMap;
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut map = HashMap::new();
+
+    for c in &s {
+        let count = map.entry(c).or_insert(0);
+        *count += 1;
+    }
+
+    for i in 1..=s.len() {
+        let mut c = 0;
+        for v in map.values() {
+            if v == &i {
+                c += 1;
+            }
+        }
+        if c != 0 && c != 2 {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
