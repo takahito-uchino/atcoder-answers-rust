592c9fd81f9d5117aa282a7afe3e4a4372c90dc2
Taka <takahito.uchino.1218@gmail.com>
2024-04-13 13:47:14 +0900

    tessoku A22

--- tessoku-book/src/bin/a22.rs
+++ tessoku-book/src/bin/a22.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [isize; n - 1],
+        b: [isize; n - 1],
+    }
+
+    let mut dp = vec![-1000000000; n];
+    dp[0] = 0;
+
+    for i in 0..n - 1 {
+        dp[a[i] as usize - 1] = (dp[a[i] as usize - 1]).max(dp[i] + 100);
+        dp[b[i] as usize - 1] = (dp[b[i] as usize - 1]).max(dp[i] + 150);
+    }
+
+    println!("{}", dp[n - 1]);
 }
