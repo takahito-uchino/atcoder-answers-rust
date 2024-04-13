3cdec3ae5393675a30997974052cee215952eac2
Taka <takahito.uchino.1218@gmail.com>
2024-04-13 14:00:19 +0900

    tessoku A16

--- tessoku-book/src/bin/a16.rs
+++ tessoku-book/src/bin/a16.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n - 1],
+        b: [usize; n - 2],
+    }
+
+    let mut dp = vec![2000000000; n];
+    dp[0] = 0;
+
+    for i in 0..n - 2 {
+        dp[i + 1] = dp[i + 1].min(dp[i] + a[i]);
+        dp[i + 2] = dp[i + 2].min(dp[i] + b[i]);
+    }
+    dp[n - 1] = dp[n - 1].min(dp[n - 2] + a[n - 2]);
+
+    println!("{}", dp[n - 1]);
 }
