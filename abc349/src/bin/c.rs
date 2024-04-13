5811d2d0d5676bdb1f14c42898d467bb08d54aa1
Taka <takahito.uchino.1218@gmail.com>
2024-04-13 21:43:25 +0900

    ABC349 C

--- abc349/src/bin/c.rs
+++ abc349/src/bin/c.rs
@@ -1,3 +1,50 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    if t[2] == 'X' {
+        let mut dp = vec![vec![0; 3]; s.len() + 1];
+        for i in 0..=s.len() {
+            for j in 0..=2 {
+                if i >= 1 && j >= 1 && s[i - 1].to_uppercase().to_string().chars().next().unwrap() == t[j- 1] {
+                    dp[i][j] = (dp[i - 1][j]).max(dp[i][j - 1]).max(dp[i - 1][j - 1] + 1);
+                } else if i >= 1 && j >= 1 {
+                    dp[i][j] = (dp[i - 1][j]).max(dp[i][j - 1]);
+                } else if i >= 1 {
+                    dp[i][j] = dp[i - 1][j];
+                } else if j >= 1 {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        if dp[s.len()][2] > 1 {
+            println!("Yes");
+            return;
+        }
+    }
+
+    let mut dp = vec![vec![0; 4]; s.len() + 1];
+    for i in 0..=s.len() {
+        for j in 0..=3 {
+            if i >= 1 && j >= 1 && s[i - 1].to_uppercase().to_string().chars().next().unwrap() == t[j - 1] {
+                dp[i][j] = (dp[i - 1][j]).max(dp[i][j - 1]).max(dp[i - 1][j - 1] + 1);
+            } else if i >= 1 && j >= 1 {
+                dp[i][j] = (dp[i - 1][j]).max(dp[i][j - 1]);
+            } else if i >= 1 {
+                dp[i][j] = dp[i - 1][j];
+            } else if j >= 1 {
+                dp[i][j] = dp[i][j - 1];
+            }
+        }
+    }
+
+    if dp[s.len()][3] > 2 {
+        println!("Yes");
+        return;
+    }
+    println!("No");
 }
