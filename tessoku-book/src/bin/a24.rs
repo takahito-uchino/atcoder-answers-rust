e46b5fd9d6ed9bd14c4dcd7b947ccc5a11dede84
Taka <takahito.uchino.1218@gmail.com>
2024-04-14 13:20:09 +0900

    tessoku A24

--- tessoku-book/src/bin/a24.rs
+++ tessoku-book/src/bin/a24.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut dp = vec![0; n];
+    let mut len = 0;
+    let mut l = Vec::new();
+
+    for i in 0..n {
+        let pos = l.binary_search(&a[i]).unwrap_or_else(|x| x);
+        dp[i] = pos;
+
+        if dp[i] >= len {
+            l.push(a[i]);
+            len += 1;
+        } else {
+            l[pos] = a[i];
+        }
+    }
+
+    println!("{}", len);
 }
