a605d1c256788abf2120f8c91f548125035f853c
Taka <takahito.uchino.1218@gmail.com>
2024-04-15 13:26:20 +0900

    tessoku A25

--- tessoku-book/src/bin/a25.rs
+++ tessoku-book/src/bin/a25.rs
@@ -1,3 +1,29 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        c: [Chars; h],
+    }
+
+    let mut dp = vec![vec![0usize; w]; h];
+
+    for i in 0..h {
+        for j in 0..w {
+            if i == 0 && j == 0 {
+                dp[i][j] = 1;
+            } else {
+                dp[i][j] = 0;
+                if i >= 1 && c[i - 1][j] == '.' {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if j >= 1 && c[i][j - 1] == '.' {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+    }
+
+    println!("{}", dp[h - 1][w - 1]);
 }
