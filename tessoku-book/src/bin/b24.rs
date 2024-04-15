497edd5c3e0728d45165bf0a7a16b2fea1be6a1d
Taka <takahito.uchino.1218@gmail.com>
2024-04-15 13:11:04 +0900

    tessoku B24

--- tessoku-book/src/bin/b24.rs
+++ tessoku-book/src/bin/b24.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        xy: [(isize, isize); n],
+    }
+
+    let mut coordinates = xy.iter().map(|&(x, y)| (x, -y)).collect::<Vec<_>>();
+    coordinates.sort_unstable();
+
+    let sorted_y = coordinates.iter().map(|&(_, y)| -y).collect::<Vec<isize>>();
+
+    let lis_length = get_lis_value(&sorted_y);
+    println!("{}", lis_length);
+}
+
+fn get_lis_value(a: &[isize]) -> usize {
+    let mut lis = vec![];
+
+    for &item in a {
+        match lis.binary_search(&item) {
+            Ok(_) => continue,
+            Err(pos) => {
+                if pos == lis.len() {
+                    lis.push(item);
+                } else {
+                    lis[pos] = item;
+                }
+            },
+        }
+    }
+    lis.len()
 }
