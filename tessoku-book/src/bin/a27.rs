35f3e13c9f18eb2f5f621b82503d579d088304ad
Taka <takahito.uchino.1218@gmail.com>
2024-04-16 14:20:30 +0900

    tessoku A27

--- tessoku-book/src/bin/a27.rs
+++ tessoku-book/src/bin/a27.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut a: usize,
+        mut b: usize,
+    }
+
+    while a >= 1 && b >= 1 {
+        if a >= b {
+            a %= b;
+        } else {
+            b %= a;
+        }
+    }
+
+    println!("{}", if a != 0 { a } else { b })
 }
