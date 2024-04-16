e8fe3d27f0b3c87be1ff2ec4243371156cf60e84
Taka <takahito.uchino.1218@gmail.com>
2024-04-16 14:20:38 +0900

    tessoku B27

--- tessoku-book/src/bin/b27.rs
+++ tessoku-book/src/bin/b27.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+    }
+
+    let mut tmp_a = a;
+    let mut tmp_b = b;
+    while tmp_a >= 1 && tmp_b >= 1 {
+        if tmp_a >= tmp_b {
+            tmp_a %= tmp_b;
+        } else {
+            tmp_b %= tmp_a;
+        }
+    }
+
+    println!("{}", if tmp_a != 0 {a * b / tmp_a} else { a * b / tmp_b });
 }
