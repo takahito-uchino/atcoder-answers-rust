edcae72ab8843ddb102c669e23a4277ea2e1fa21
Taka <takahito.uchino.1218@gmail.com>
2024-04-16 13:56:21 +0900

    tessoku A26

--- tessoku-book/src/bin/a26.rs
+++ tessoku-book/src/bin/a26.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+        x: [usize; q],
+    }
+
+    let mut deleted = vec![false; 300001];
+
+    for i in 2..=((300000 as f64).sqrt() as usize) {
+        if deleted[i] {
+            continue;
+        }
+        for j in (i * 2..=300000).step_by(i) {
+            deleted[j] = true;
+        }
+    }
+
+    for xi in x {
+        println!("{}", if !deleted[xi] { "Yes" } else { "No" })
+    }
 }
