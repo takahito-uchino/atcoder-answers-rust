c6c3f5aafc068eb3a7d4dd72c6b37c62689fb319
Taka <takahito.uchino.1218@gmail.com>
2024-04-17 13:38:53 +0900

    tessoku A28

--- tessoku-book/src/bin/a28.rs
+++ tessoku-book/src/bin/a28.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        ta: [(String, isize); n],
+    }
+
+    let mut t = Vec::new();
+    let mut a = Vec::new();
+
+    for (ti, ai) in ta {
+        t.push(ti);
+        a.push(ai);
+    }
+
+    let mut answer = 0;
+
+    for i in 0..n {
+        if t[i] == "+" {
+            answer += a[i];
+        } else if t[i] == "-" {
+            answer -= a[i];
+        } else {
+            answer *= a[i];
+        }
+
+        answer %= 10000;
+        if answer < 0 {
+            println!("{}", 10000 + answer);
+        } else {
+            println!("{}", answer);
+        }
+    }
 }
