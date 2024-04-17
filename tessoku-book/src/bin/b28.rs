74330c8b9d4ac0d29498665a21bb6ffeb051bda0
Taka <takahito.uchino.1218@gmail.com>
2024-04-17 13:39:18 +0900

    tessoku B28

--- tessoku-book/src/bin/b28.rs
+++ tessoku-book/src/bin/b28.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    let mut a = Vec::new();
+
+    a.push(1);
+    a.push(1);
+
+    for i in 2..n {
+        a.push((a[i - 1] + a[i - 2]) % 1000000007);
+    }
+
+    println!("{}", a[n - 1]);
 }
