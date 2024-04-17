659d991669176b67ed7b5169826893bf21a87e66
Taka <takahito.uchino.1218@gmail.com>
2024-04-17 13:39:32 +0900

    tessoku B29

--- tessoku-book/src/bin/b29.rs
+++ tessoku-book/src/bin/b29.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+    }
+
+    let m = 1000000007;
+
+    let mut p = a % m;
+    let mut answer = 1;
+
+    for i in 0..60 {
+        let wari = 1 << i;
+        if (b / wari) % 2 == 1 {
+            answer = (answer * p) % m;
+        }
+        p = (p * p) % m;
+    }
+
+    println!("{}", answer);
 }
