d2f4c68e1c85066e8e94df4906fd75272cb0fa35
Taka <takahito.uchino.1218@gmail.com>
2024-04-18 13:31:37 +0900

    tessoku B30

--- tessoku-book/src/bin/b30.rs
+++ tessoku-book/src/bin/b30.rs
@@ -1,3 +1,46 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+    }
+
+    let m = 1000000007;
+    let mut a = 1;
+
+    for i in 1..=(h + w - 2) {
+        a = (a * i) % m;
+    }
+
+    let mut b = 1;
+
+    for i in 1..h {
+        b = (b * i) % m;
+    }
+
+    for i in 1..w {
+        b = (b * i) % m;
+    }
+
+    println!("{}", division(a, b, m));
+}
+
+fn power(a: usize, b: usize, m: usize) -> usize {
+    let mut p = a;
+    let mut answer = 1;
+
+    for i in 0..30 {
+        let wari = 1 << i;
+        if (b / wari) % 2 == 1 {
+            answer = (answer * p) % m;
+        }
+        p = (p * p) % m;
+    }
+
+    answer
+}
+
+fn division(a: usize, b: usize, m: usize) -> usize {
+    (a * power(b, m - 2, m)) % m
 }
