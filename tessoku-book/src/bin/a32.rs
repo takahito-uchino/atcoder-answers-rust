33248ca300145ac83bfdc7eda207ae03c4164052
Taka <takahito.uchino.1218@gmail.com>
2024-04-18 13:31:57 +0900

    tessoku A32

--- tessoku-book/src/bin/a32.rs
+++ tessoku-book/src/bin/a32.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+    }
+
+    let mut dp = vec![false; n + 1];
+    dp[a] = true;
+
+    for i in a..=n {
+        if !dp[i - a] {
+            dp[i] = true;
+        } else if i >= b && !dp[i - b] {
+            dp[i] = true;
+        } else {
+            dp[i] = false;
+        }
+    }
+
+    println!("{}", if dp[n] { "First" } else { "Second" })
 }
