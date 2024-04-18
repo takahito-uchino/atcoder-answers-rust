9da24f47f4942da14db1556fa5d58661a4d2d47d
Taka <takahito.uchino.1218@gmail.com>
2024-04-18 13:32:04 +0900

    tessoku B32

--- tessoku-book/src/bin/b32.rs
+++ tessoku-book/src/bin/b32.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; k],
+    }
+
+    let mut sorted_a = a.clone();
+    sorted_a.sort();
+    let mut dp = vec![false; n + 1];
+    let a1 = sorted_a[0];
+
+    for i in a1..=n {
+        for ai in &sorted_a {
+            if i >= *ai && !dp[i - ai] {
+                dp[i] = true;
+            }
+        }
+    }
+
+    println!("{}", if dp[n] { "First" } else { "Second" })
 }
