8bd943776439826c28a9e0aec2b99d2752b24851
Taka <takahito.uchino.1218@gmail.com>
2024-04-18 13:31:52 +0900

    tessoku B31

--- tessoku-book/src/bin/b31.rs
+++ tessoku-book/src/bin/b31.rs
@@ -1,3 +1,9 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    println!("{}", (n / 3) + (n / 5) + (n / 7) - (n / 15) - (n / 35) - (n / 21) + (n / 105))
 }
