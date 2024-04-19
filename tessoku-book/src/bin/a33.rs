ee23294c3cf49bcd7c87f6db1e02da74a4e4d006
Taka <takahito.uchino.1218@gmail.com>
2024-04-19 14:46:41 +0900

    tessoku A33

--- tessoku-book/src/bin/a33.rs
+++ tessoku-book/src/bin/a33.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut xor_sum = a[0];
+
+    for i in 1..n {
+        xor_sum = xor_sum ^ a[i];
+    }
+
+    println!("{}", if xor_sum != 0 { "First" } else { "Second" })
 }
