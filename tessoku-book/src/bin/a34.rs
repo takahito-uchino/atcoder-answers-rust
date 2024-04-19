a911d02060d11b08e9a93db42943cf2f91ccb178
Taka <takahito.uchino.1218@gmail.com>
2024-04-19 14:46:58 +0900

    tessoku A34

--- tessoku-book/src/bin/a34.rs
+++ tessoku-book/src/bin/a34.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: usize,
+        y: usize,
+        a: [usize; n],
+    }
+
+    let mut grundy = vec![0; 100001];
+
+    for i in 0..=100000 {
+        let mut transit = vec![false, false, false];
+        if i >= x {
+            transit[grundy[i - x]] = true;
+        }
+        if i >= y {
+            transit[grundy[i - y]] = true;
+        }
+        if !transit[0] {
+            grundy[i] = 0;
+        } else if !transit[1] {
+            grundy[i] = 1;
+        } else {
+            grundy[i] = 2;
+        }
+    }
+
+    let mut xor_sum = 0;
+
+    for i in 0..n {
+        xor_sum = xor_sum ^ grundy[a[i]];
+    }
+
+    println!("{}", if xor_sum != 0 {"First"} else {"Second"})
 }
