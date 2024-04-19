e93d187e4e8438893b63bf2dc2091e778f3c7601
Taka <takahito.uchino.1218@gmail.com>
2024-04-19 14:47:04 +0900

    tessoku B34

--- tessoku-book/src/bin/b34.rs
+++ tessoku-book/src/bin/b34.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        _: usize,
+        _: usize,
+        a: [usize; n],
+    }
+
+    let grundy = vec![0, 0, 1, 1, 2];
+
+    let mut xor_sum = 0;
+
+    for ai in a {
+        xor_sum ^= grundy[ai % 5];
+    }
+
+    println!("{}", if xor_sum != 0 { "First" } else { "Second" })
 }
