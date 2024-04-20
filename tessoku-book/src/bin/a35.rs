5233fc2d0d1cdcf91f9c9254383852e6b2030dff
Taka <takahito.uchino.1218@gmail.com>
2024-04-20 10:09:13 +0900

    tessoku A35

--- tessoku-book/src/bin/a35.rs
+++ tessoku-book/src/bin/a35.rs
@@ -1,3 +1,28 @@
+use std::cmp::{max, min};
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut dp = vec![vec![0; n + 1]; n + 1];
+
+    for i in 0..n {
+        dp[n][i + 1] = a[i];
+    }
+
+    for i in (1..=n - 1).rev() {
+        for j in 1..=i {
+            if i % 2 == 1 {
+                dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1]);
+            } else {
+                dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+    }
+
+    println!("{}", dp[1][1])
 }
