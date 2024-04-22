42c208704c5e8c62a966b5da5a9e1f41ffec14f9
Taka <takahito.uchino.1218@gmail.com>
2024-04-22 17:29:47 +0900

    tessoku B36

--- tessoku-book/src/bin/b36.rs
+++ tessoku-book/src/bin/b36.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        _: usize,
+        k: usize,
+        s: String,
+    }
+
+    let mut on_count = 0;
+
+    for c in s.chars() {
+        if c == '1' {
+            on_count +=1;
+        }
+    }
+
+    if on_count % 2 == k % 2 {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
