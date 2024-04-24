6a4b1a7c5832c06d4896acbfb4aa72477c9347d7
Taka <takahito.uchino.1218@gmail.com>
2024-04-24 15:42:29 +0900

    tessoku A37

--- tessoku-book/src/bin/a37.rs
+++ tessoku-book/src/bin/a37.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        b: usize,
+        a: [usize; n],
+        c: [usize; m],
+    }
+
+    let a_sum = a.iter().sum::<usize>();
+    let c_sum = c.iter().sum::<usize>();
+
+    let answer = a_sum * m + b * n * m + c_sum * n;
+
+    println!("{}", answer);
 }
