fec0547e3efd567fd0e3e192d02b41fc6c9545ef
Taka <takahito.uchino.1218@gmail.com>
2024-04-24 15:42:47 +0900

    tessoku A38

--- tessoku-book/src/bin/a38.rs
+++ tessoku-book/src/bin/a38.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        d: usize,
+        n: usize,
+        lrh: [(usize, usize, usize); n],
+    }
+
+    let mut hours = vec![24; d];
+
+    for (l, r, h) in lrh {
+        for i in (l - 1)..r {
+            hours[i] = hours[i].min(h);
+        }
+    }
+
+    let answer = hours.iter().sum::<usize>();
+
+    println!("{}", answer);
 }
