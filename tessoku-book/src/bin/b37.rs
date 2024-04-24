b117bbd8a120cbdf186ebdb986edf88b2e4fe1b3
Taka <takahito.uchino.1218@gmail.com>
2024-04-24 15:42:39 +0900

    tessoku B37

--- tessoku-book/src/bin/b37.rs
+++ tessoku-book/src/bin/b37.rs
@@ -1,3 +1,38 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    let mut power_10 = Vec::new();
+    for i in 0..17 {
+        power_10.push(10usize.pow(i));
+    }
+
+    let mut r = vec![vec![None; 10]; 17];
+
+    for i in 0..16 {
+        let digit = (n / power_10[i]) % 10;
+
+        for j in 0..10 {
+            if j < digit {
+                r[i][j] = Some((n / power_10[i + 1] + 1) * power_10[i]);
+            } else if j == digit {
+                r[i][j] = Some((n / power_10[i + 1]) * power_10[i] + (n % power_10[i]) + 1);
+            } else {
+                r[i][j] = Some((n / power_10[i + 1]) * power_10[i]);
+            }
+        }
+    }
+
+    let mut answer = 0;
+
+    for i in 0..16 {
+        for j in 0..10 {
+            answer += j * r[i][j].unwrap();
+        }
+    }
+
+    println!("{}", answer);
 }
