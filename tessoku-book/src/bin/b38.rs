4ffb0cd6304eeaebabd2f0dff11a93a52bfadd10
Taka <takahito.uchino.1218@gmail.com>
2024-04-24 15:42:52 +0900

    tessoku B38

--- tessoku-book/src/bin/b38.rs
+++ tessoku-book/src/bin/b38.rs
@@ -1,3 +1,43 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    let mut ret1 = vec![None; n];
+    let mut ret2 = vec![None; n];
+
+    let mut streak1 = 1;
+    ret1[0] = Some(1);
+
+    for i in 0..n-1 {
+        if s[i] == 'A' {
+            streak1 += 1;
+        } else {
+            streak1 = 1;
+        }
+        ret1[i + 1] = Some(streak1);
+    }
+
+    let mut streak2 = 1;
+    ret2[n - 1] = Some(1);
+
+    for i in (0..n-1).rev() {
+        if s[i] == 'A' {
+            streak2 = 1;
+        } else {
+            streak2 += 1;
+        }
+        ret2[i] = Some(streak2);
+    }
+
+    let mut answer = 0;
+
+    for i in 0..n {
+        answer += ret1[i].unwrap().max(ret2[i].unwrap());
+    }
+
+    println!("{}", answer);
 }
