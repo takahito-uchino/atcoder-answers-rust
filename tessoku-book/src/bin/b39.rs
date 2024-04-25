18793831c53fcd95b1378ee59de6bdcae630bae2
Taka <takahito.uchino.1218@gmail.com>
2024-04-25 15:05:29 +0900

    tessoku B39

--- tessoku-book/src/bin/b39.rs
+++ tessoku-book/src/bin/b39.rs
@@ -1,3 +1,33 @@
+use std::usize;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        d: usize,
+        mut xy: [(usize, usize); n],
+    }
+
+    let mut used = vec![false; n];
+    let mut answer = 0;
+
+    for i in 1..=d {
+        let mut max_value = 0;
+        let mut max_id = usize::MAX;
+
+        for j in 0..n {
+            if !used[j] && max_value < xy[j].1 && xy[j].0 <= i {
+                max_value = xy[j].1;
+                max_id = j;
+            }
+        }
+
+        if max_id != usize::MAX {
+            answer += max_value;
+            used[max_id] = true;
+        }
+    }
+
+    println!("{}", answer);
 }
