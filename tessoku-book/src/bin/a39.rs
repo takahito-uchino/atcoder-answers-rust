05cd2f2d93b1f664f0cabccbcd0d4c9c2f7166af
Taka <takahito.uchino.1218@gmail.com>
2024-04-25 15:05:17 +0900

    tessoku A39

--- tessoku-book/src/bin/a39.rs
+++ tessoku-book/src/bin/a39.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut lr: [(usize, usize); n],
+    }
+
+    lr.sort_by_key(|k| k.1);
+
+    let mut current_time = 0;
+    let mut answer = 0;
+
+    for i in 0..n {
+        if current_time <= lr[i].0 {
+            current_time = lr[i].1;
+            answer += 1;
+        }
+    }
+
+    println!("{}", answer);
 }
