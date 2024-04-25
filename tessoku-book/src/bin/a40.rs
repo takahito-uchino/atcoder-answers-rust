a5a3ab1ca6a640cd62cbf2f0d74c82c3102f191d
Taka <takahito.uchino.1218@gmail.com>
2024-04-25 15:05:37 +0900

    tessoku A40

--- tessoku-book/src/bin/a40.rs
+++ tessoku-book/src/bin/a40.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut counts = vec![0; 101];
+
+    for i in 0..n {
+        counts[a[i]] += 1;
+    }
+
+    let mut answer = 0;
+
+    for i in 1..=100 {
+        if counts[i] >= 3 {
+            answer += counts[i] * (counts[i] - 1) * (counts[i] - 2) / 6;
+        }
+    }
+
+    println!("{}", answer);
 }
