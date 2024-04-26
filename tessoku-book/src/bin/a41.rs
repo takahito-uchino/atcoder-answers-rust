1312edf5d00f3fbb39b63a2a8d381b91f144ddf4
Taka <takahito.uchino.1218@gmail.com>
2024-04-26 16:02:14 +0900

    tessoku A41

--- tessoku-book/src/bin/a41.rs
+++ tessoku-book/src/bin/a41.rs
@@ -1,3 +1,21 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    let mut answer = false;
+
+    for i in 0..n-2 {
+        if s[i]=='R' && s[i+1] == 'R' && s[i+2] == 'R' {
+            answer = true;
+        }
+        if s[i]=='B' && s[i+1] == 'B' && s[i+2] == 'B' {
+            answer = true;
+        }
+    }
+
+    println!("{}", if answer {"Yes"} else {"No"})
 }
