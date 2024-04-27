ab7631dc50a216ff1017b2f615e9c57b7860d285
Taka <takahito.uchino.1218@gmail.com>
2024-04-27 16:22:10 +0900

    tessoku B41

--- tessoku-book/src/bin/b41.rs
+++ tessoku-book/src/bin/b41.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut x: usize,
+        mut y: usize,
+    }
+
+    let mut answer = Vec::new();
+
+    loop {
+        if x == 1 && y == 1 {
+            break;
+        }
+        answer.push((x, y));
+        if x > y {
+            x = x - y;
+        } else {
+            y = y - x;
+        }
+    }
+
+    answer.reverse();
+
+    println!("{}", answer.len());
+    for (l, r) in answer {
+        println!("{} {}", l, r);
+    }
 }
