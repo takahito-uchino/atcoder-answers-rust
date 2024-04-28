58e6177c2c1af5031239c9a318a73c119a226581
Taka <takahito.uchino.1218@gmail.com>
2024-04-28 17:45:19 +0900

    tessoku B42

--- tessoku-book/src/bin/b42.rs
+++ tessoku-book/src/bin/b42.rs
@@ -1,3 +1,46 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    let mut a = Vec::new();
+    let mut b = Vec::new();
+
+    for _ in 0..n {
+        input! {
+            ai: isize,
+            bi: isize,
+        }
+        a.push(ai);
+        b.push(bi);
+    }
+
+    let answer1 = solve(1, 1, n, &a, &b);
+    let answer2 = solve(1, 2, n, &a, &b);
+    let answer3 = solve(2, 1, n, &a, &b);
+    let answer4 = solve(2, 2, n, &a, &b);
+
+    println!("{}", answer1.max(answer2).max(answer3).max(answer4))
+}
+
+fn solve(omote: isize, ura: isize, n: usize, a: &Vec<isize>, b: &Vec<isize>) -> isize {
+    let mut sum = 0;
+
+    for i in 0..n {
+        let mut card1 = a[i];
+        if omote == 2 {
+            card1 = -a[i];
+        }
+        let mut card2 = b[i];
+        if ura == 2 {
+            card2 = -b[i]
+        }
+        if card1 + card2 >= 0 {
+            sum += card1 + card2;
+        }
+    }
+
+    sum
 }
