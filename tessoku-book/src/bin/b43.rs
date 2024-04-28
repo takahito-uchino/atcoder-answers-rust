93ae589283f49e6b92c217cb6c8d6d87a20ee5ed
Taka <takahito.uchino.1218@gmail.com>
2024-04-28 17:45:32 +0900

    tessoku B43

--- tessoku-book/src/bin/b43.rs
+++ tessoku-book/src/bin/b43.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; m],
+    }
+
+    let mut answer = vec![0; n];
+
+    for i in 0..m {
+        answer[a[i] - 1] += 1;
+    }
+
+    for i in answer {
+        println!("{}", m - i)
+    }
 }
