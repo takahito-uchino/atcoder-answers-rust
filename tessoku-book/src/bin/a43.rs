4fa6acc97fa82af60170d75889607c24b0f5d98c
Taka <takahito.uchino.1218@gmail.com>
2024-04-28 17:45:26 +0900

    tessoku A43

--- tessoku-book/src/bin/a43.rs
+++ tessoku-book/src/bin/a43.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        l: usize,
+        ab: [(usize, String); n],
+    }
+
+    let mut answer = 0;
+
+    for i in 0..n {
+        if ab[i].1 == "E".to_string() {
+            answer = answer.max(l - ab[i].0);
+        } else {
+            answer = answer.max(ab[i].0);
+        }
+    }
+
+    println!("{}", answer)
 }
