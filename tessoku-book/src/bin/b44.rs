e8f4eddb718ef039cb2945b8e73fb2cba09ea6c0
Taka <takahito.uchino.1218@gmail.com>
2024-04-29 15:47:35 +0900

    tessoku B44

--- tessoku-book/src/bin/b44.rs
+++ tessoku-book/src/bin/b44.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [[usize; n]; n],
+        q: usize,
+        queries: [(usize, usize, usize); q],
+    }
+
+    let mut t = Vec::new();
+
+    for i in 0..n {
+        t.push(i);
+    }
+
+    for (i, x, y) in queries {
+        if i == 1 {
+            (t[x - 1], t[y - 1]) = (t[y - 1], t[x - 1]);
+        } else {
+            println!("{}", a[t[x - 1]][y - 1])
+        }
+    }
 }
