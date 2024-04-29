b6e3e4350f68ec721a799a006f432267709c682a
Taka <takahito.uchino.1218@gmail.com>
2024-04-29 15:47:17 +0900

    tessoku A44

--- tessoku-book/src/bin/a44.rs
+++ tessoku-book/src/bin/a44.rs
@@ -1,3 +1,51 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+    }
+
+    let mut a = Vec::new();
+
+    for i in 0..n {
+        a.push(i + 1);
+    }
+
+    let mut reverse = false;
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
+
+        if t == 1 {
+            input! {
+                x: usize,
+                y: usize,
+            }
+
+            if !reverse {
+                a[x - 1] = y;
+            } else {
+                a[n - x] = y;
+            }
+        }
+
+        if t == 2 {
+            reverse = if !reverse { true } else { false };
+        }
+
+        if t == 3 {
+            input! {
+                x: usize
+            }
+
+            if !reverse {
+                println!("{}", a[x - 1])
+            } else {
+                println!("{}", a[n - x])
+            }
+        }
+    }
 }
