bcf8029699761a1c76111b8441fd8d8f1d130c46
Taka <takahito.uchino.1218@gmail.com>
2024-04-30 14:52:57 +0900

    tessoku A45

--- tessoku-book/src/bin/a45.rs
+++ tessoku-book/src/bin/a45.rs
@@ -1,3 +1,31 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        c: String,
+        a: Chars,
+    }
+
+    let mut score = 0;
+
+    for i in 0..n {
+        if a[i] == 'W' {
+            score += 0;
+        } else if a[i] == 'B' {
+            score += 1;
+        } else if a[i] == 'R' {
+            score += 2;
+        }
+    }
+
+    if score % 3 == 0 && c == "W" {
+        println!("Yes")
+    } else if score % 3 == 1 && c == "B" {
+        println!("Yes")
+    } else if score % 3 == 2 && c == "R" {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
