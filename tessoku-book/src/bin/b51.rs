445734b6c25dda6f02aeb16f2776b689723f8668
Taka <takahito.uchino.1218@gmail.com>
2024-05-01 14:29:33 +0900

    tessoku B51

--- tessoku-book/src/bin/b51.rs
+++ tessoku-book/src/bin/b51.rs
@@ -1,3 +1,17 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut answer = Vec::new();
+
+    for i in 0..s.len() {
+        if s[i] == '(' {
+            answer.push(i);
+        } else {
+            println!("{} {}", answer.pop().unwrap() + 1, i + 1);
+        }
+    }
 }
