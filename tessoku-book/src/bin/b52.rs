004a8a668b5d86e1ea422613857f52edc43c5561
Taka <takahito.uchino.1218@gmail.com>
2024-05-01 14:29:49 +0900

    tessoku B52

--- tessoku-book/src/bin/b52.rs
+++ tessoku-book/src/bin/b52.rs
@@ -1,3 +1,29 @@
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: usize,
+        mut a: Chars,
+    }
+
+    let mut queue = VecDeque::new();
+    queue.push_back(x - 1);
+    a[x - 1] = '@';
+
+    while !queue.is_empty() {
+        let pos = queue.pop_front().unwrap();
+        if pos > 0 &&a[pos - 1] == '.' {
+            a[pos - 1] = '@';
+            queue.push_back(pos - 1);
+        }
+        if pos + 1 < n && a[pos + 1] == '.' {
+            a[pos + 1] = '@';
+            queue.push_back(pos + 1);
+        }
+    }
+
+    println!("{}", a.into_iter().collect::<String>());
 }
