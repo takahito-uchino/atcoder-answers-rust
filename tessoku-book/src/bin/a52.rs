11cf6227cee64a9b1c135497bcd6c765c05e3ba0
Taka <takahito.uchino.1218@gmail.com>
2024-05-01 14:29:41 +0900

    tessoku A52

--- tessoku-book/src/bin/a52.rs
+++ tessoku-book/src/bin/a52.rs
@@ -1,3 +1,28 @@
+use std::collections::VecDeque;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+    }
+
+    let mut answer = VecDeque::new();
+
+    for _ in 0..q {
+        input! {
+            n: usize,
+        }
+
+        if n == 1 {
+            input! {
+                name: String,
+            }
+            answer.push_back(name);
+        } else if n == 2 {
+            println!("{}", answer[0]);
+        } else if n == 3 {
+            answer.pop_front();
+        }
+    }
 }
