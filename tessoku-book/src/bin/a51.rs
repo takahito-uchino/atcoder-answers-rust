c1f61dc4289e2e06ea217d75eeaa450af3eaa990
Taka <takahito.uchino.1218@gmail.com>
2024-05-01 14:29:25 +0900

    tessoku A51

--- tessoku-book/src/bin/a51.rs
+++ tessoku-book/src/bin/a51.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+    }
+
+    let mut books = Vec::new();
+
+    for _ in 0..q {
+        input! {
+            n: usize,
+        }
+
+        if n == 1 {
+            input! {
+                title: String,
+            }
+            books.push(title);
+        } else if n == 2 {
+            println!("{}", books[books.len() - 1]);
+        } else if n == 3 {
+            books.pop();
+        }
+
+    }
 }
