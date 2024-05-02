f61749c5f92c60666d06cfef9ae5d6758d76f5dc
Taka <takahito.uchino.1218@gmail.com>
2024-05-02 15:03:55 +0900

    tessoku A53

--- tessoku-book/src/bin/a53.rs
+++ tessoku-book/src/bin/a53.rs
@@ -1,3 +1,29 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+    }
+
+    let mut priority_queue = BinaryHeap::new();
+
+    for _ in 0..q {
+        input! {
+            n: usize,
+        }
+
+        if n == 1 {
+            input! {
+                price: usize,
+            }
+
+            priority_queue.push(Reverse(price));
+        } else if n == 2 {
+            println!("{}", priority_queue.peek().unwrap().0);
+        } else if n == 3 {
+            priority_queue.pop();
+        }
+    }
 }
