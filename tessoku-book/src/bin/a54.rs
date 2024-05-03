949271a0c661164e27077b3ab117ac4e3aee235e
Taka <takahito.uchino.1218@gmail.com>
2024-05-03 16:35:11 +0900

    tessoku A54

--- tessoku-book/src/bin/a54.rs
+++ tessoku-book/src/bin/a54.rs
@@ -1,3 +1,32 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+    }
+
+    let mut map = HashMap::new();
+
+    for _ in 0..q {
+        input! {
+            n: usize,
+        }
+
+        if n == 1 {
+            input! {
+                name: String,
+                score: usize,
+            }
+
+            map.insert(name, score);
+        } else {
+            input! {
+                name: String,
+            }
+
+            println!("{}", map.get(&name).unwrap())
+        }
+    }
 }
