47350dc73c817bcee131963833fd8817d2b96ef8
Taka <takahito.uchino.1218@gmail.com>
2024-05-03 16:35:25 +0900

    tessoku A55

--- tessoku-book/src/bin/a55.rs
+++ tessoku-book/src/bin/a55.rs
@@ -1,3 +1,30 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+    }
+
+    let mut table = HashSet::new();
+
+    for _ in 0..q {
+        input! {
+            n: usize,
+            num: usize,
+        }
+
+        if n == 1 {
+            table.insert(num);
+        } else if n == 2 {
+            table.remove(&num);
+        } else if n == 3 {
+            let itr = table.iter().filter(|&&m| m >= num).min();
+            match itr {
+                Some(&min) => println!("{}", min),
+                None => println!("-1"),
+            }
+        }
+    }
 }
