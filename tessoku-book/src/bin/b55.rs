2c070d3656556efe6d81e6c5bfff25657604af30
Taka <takahito.uchino.1218@gmail.com>
2024-05-03 16:35:31 +0900

    tessoku B55

--- tessoku-book/src/bin/b55.rs
+++ tessoku-book/src/bin/b55.rs
@@ -1,3 +1,37 @@
+use std::collections::BTreeSet;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+    }
+
+    let mut table = BTreeSet::new();
+
+    for _ in 0..q {
+        input! {
+            n: usize,
+            x: usize,
+        }
+
+        if n == 1 {
+            table.insert(x);
+        } else if n == 2 {
+            let mut result = None;
+            if let Some(&min) = table.range(x..).next() {
+                result = Some(min - x);
+            }
+            if let Some(&max) = table.range(..=x).next_back() {
+                result = match result {
+                    Some(existing) => Some(existing.min(x - max)),
+                    None => Some(x - max),
+                };
+            }
+            match result {
+                Some(min_diff) => println!("{}", min_diff),
+                None => println!("-1"),
+            }
+        }
+    }
 }
