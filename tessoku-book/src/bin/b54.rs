d9d01a1ca7e6e7d53b810b6f240ce63fd8d1252b
Taka <takahito.uchino.1218@gmail.com>
2024-05-03 16:35:20 +0900

    tessoku B54

--- tessoku-book/src/bin/b54.rs
+++ tessoku-book/src/bin/b54.rs
@@ -1,3 +1,21 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut map = HashMap::new();
+    let mut answer = 0usize;
+
+    for &v in &a {
+        let count = map.entry(v).or_insert(0);
+        answer += *count;
+        *count += 1;
+    }
+
+    println!("{}", answer)
 }
