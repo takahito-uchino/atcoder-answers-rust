a8c1892fd0f70a23bfa40c27234d68d36707d255
Taka <takahito.uchino.1218@gmail.com>
2024-05-09 16:03:04 +0900

    ABC007

---
+++ abc007/src/bin/c.rs
@@ -0,0 +1,13 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        c: [usize; n],
+    }
+
+    let mut is_front = vec![true; n];
+    let mut sum = 0;
+
+    for i in 
+}
