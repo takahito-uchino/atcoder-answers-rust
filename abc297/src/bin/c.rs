6d2fb6f6117c8e3f78491e9eff6e81d48c96f8e8
Taka <takahito.uchino.1218@gmail.com>
2024-08-07 14:50:52 +0900

    ABC297 C

---
+++ abc297/src/bin/c.rs
@@ -0,0 +1,25 @@
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        h: usize,
+        w: usize,
+        mut s: [Chars; h],
+    }
+
+    for i in 0..h {
+        for j in 0..w - 1 {
+            if s[i][j] == 'T' && s[i][j + 1] == 'T' {
+                s[i][j] = 'P';
+                s[i][j + 1] = 'C';
+            }
+        }
+    }
+
+    for arr in s {
+        for c in arr {
+            print!("{}", c);
+        }
+        println!()
+    }
+}
