1f00f24bc14908f11a871e2e56766b1139dbea15
Taka <takahito.uchino.1218@gmail.com>
2024-08-12 17:26:23 +0900

    ABC355 C

--- abc355/src/bin/c.rs
+++ abc355/src/bin/c.rs
@@ -1,3 +1,52 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        t: usize,
+        mut a: [usize; t],
+    }
+
+    for i in 0..t {
+        a[i] -= 1;
+    }
+
+    let mut row = vec![0; n];
+    let mut col = vec![0; n];
+    let mut diag = vec![0; 2];
+
+    for i in 0..t {
+        let x = a[i] / n;
+        let y = a[i] % n;
+
+        row[x] += 1;
+        if row[x] == n {
+            println!("{}", i + 1);
+            return;
+        }
+
+        col[y] += 1;
+        if col[y] == n {
+            println!("{}", i + 1);
+            return;
+        }
+
+        if x == y {
+            diag[0] += 1;
+            if diag[0] == n {
+                println!("{}", i + 1);
+                return;
+            }
+        }
+
+        if x + y == n - 1 {
+            diag[1] += 1;
+            if diag[1] == n {
+                println!("{}", i + 1);
+                return;
+            }
+        }
+    }
+
+    println!("-1");
 }
