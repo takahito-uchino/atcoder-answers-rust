e98e76a417f40a3f76850e8547acc924904467d8
Taka <takahito.uchino.1218@gmail.com>
2024-08-14 16:13:44 +0900

    ABC360 C

--- abc360/src/bin/c.rs
+++ abc360/src/bin/c.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+        w: [usize; n],
+    }
+
+    let mut max_weight = vec![0; n];
+
+    for i in 0..n {
+        a[i] -= 1;
+        max_weight[a[i]] = max_weight[a[i]].max(w[i]);
+    }
+
+    let sum_w: usize = w.iter().sum();
+    let sum_max: usize = max_weight.iter().sum();
+
+    println!("{}", sum_w - sum_max);
 }
