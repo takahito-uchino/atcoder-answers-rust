86759562fe75598e6a233b520e62ef7ec681effa
Taka <takahito.uchino.1218@gmail.com>
2024-08-14 16:13:50 +0900

    ABC361 C

--- abc361/src/bin/c.rs
+++ abc361/src/bin/c.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        mut a: [usize; n],
+    }
+
+    a.sort_unstable();
+
+    let mut min = usize::MAX;
+
+    for i in 0..=k {
+        min = min.min(a[i + n - k - 1] - a[i]);
+    }
+
+    println!("{}", min)
 }
