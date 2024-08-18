50a497ccd722aa32b62acf98a4c831947ae0d90e
Taka <takahito.uchino.1218@gmail.com>
2024-08-18 13:50:09 +0900

    ABC242 C

---
+++ abc242/src/bin/c.rs
@@ -0,0 +1,27 @@
+use proconio::input;
+
+const MOD: usize = 998244353;
+
+fn main() {
+    input! {
+        n: usize,
+    }
+    let mut dp = vec![vec![0; 10]; n + 1];
+    for i in 1..=9 {
+        dp[1][i] = 1;
+    }
+    for d in 2..=n {
+        for i in 1..=9 {
+            for j in 1.max(i - 1)..=9.min(i + 1) {
+                dp[d][j] += dp[d - 1][i];
+                dp[d][j] %= MOD;
+            }
+        }
+    }
+    let mut ans = 0;
+    for i in 1..=9 {
+        ans += dp[n][i];
+        ans %= MOD;
+    }
+    println!("{}", ans)
+}
